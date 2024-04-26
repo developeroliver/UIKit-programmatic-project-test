738db28164214ab694a38679cd44da03a9346c4e
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-26 18:13:13 +0200

    remove storyboard

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by olivier geiger on 26/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
