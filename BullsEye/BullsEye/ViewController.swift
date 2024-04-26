4d5dd12ab00375e377c95685343185a854558cad
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-26 18:48:36 +0200

    add alert

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    lazy var showAlertButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Hit me", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        return button
+    }()
+    
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        setup()
+        layout()
     }
+    
+    
+}
 
+// MARK: - @objc Functions
+extension ViewController {
+    
+    @objc func showAlert() {
+        let alert = UIAlertController(title: "Fonctionnalité non disponible", message: "Cette fonctionnalité n'est pas encore disponible.", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
 
+// MARK: - Helpers
+extension ViewController {
+    
+    private func setup() {
+        view.backgroundColor = .systemBackground
+        
+        showAlertButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+    }
+    
+    private func layout() {
+        view.addSubview(showAlertButton)
+        showAlertButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
 }
 
