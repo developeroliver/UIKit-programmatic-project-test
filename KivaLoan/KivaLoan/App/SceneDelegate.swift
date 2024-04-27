ae89da47911fa0ef1eb78b82de17aec229448dd9
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-27 16:38:19 +0200

    remove storyboard and create MVC architecture

---
+++ KivaLoan/KivaLoan/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  KivaLoan
+//
+//  Created by olivier geiger on 27/04/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ KivaLoan/KivaLoan/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KivaLoan
+//
+//  Created by olivier geiger on 27/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
