2f884710b1fc750819fad106e995363db4c2a5aa
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-27 15:26:34 +0200

    polish the ui

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -17,21 +17,31 @@ class ViewController: UIViewController {
     var round = 0
     
     // MARK: - UI
+    lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Background")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "Put the Bull's Eye as close as you can to: "
+        label.textColor = .white
         return label
     }()
     
     lazy var goalLabel: UILabel = {
         let label = UILabel()
         label.text = "100"
+        label.textColor = .white
         return label
     }()
     
     lazy var leadingSliderLabel: UILabel = {
         let label = UILabel()
         label.text = "1"
+        label.textColor = .white
         return label
     }()
     
@@ -46,6 +56,7 @@ class ViewController: UIViewController {
     lazy var trailingSliderLabel: UILabel = {
         let label = UILabel()
         label.text = "100"
+        label.textColor = .white
         return label
     }()
     
@@ -62,44 +73,45 @@ class ViewController: UIViewController {
     
     lazy var showAlertButton: UIButton = {
         let button = UIButton(type: .custom)
-        button.setTitle("Hit me", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
+        button.setBackgroundImage(UIImage(named: "Button-Normal"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "Button-Highlighted"), for: .highlighted)
+        button.setTitle("Hit Me", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return button
+    }()
+    
+    lazy var contentStartButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setBackgroundImage(UIImage(named: "SmallButton"), for: .normal)
         return button
     }()
     
     lazy var startButton: UIButton = {
         let button = UIButton(type: .custom)
-        button.setTitle("Start Over", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
+        button.setImage(UIImage(named: "StartOverIcon"), for: .normal)
         return button
     }()
     
     lazy var scoreLabel: UILabel = {
         let label = UILabel()
         label.text = "Score: "
+        label.textColor = .white
         return label
     }()
     
     lazy var roundLabel: UILabel = {
         let label = UILabel()
         label.text = "Round: "
+        label.textColor = .white
         return label
-    }()
-    
-    lazy var infoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "info.circle")
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        return imageView
     }()
     
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            startButton,
+            contentStartButton,
             scoreLabel,
-            roundLabel,
-            infoImageView
+            roundLabel
         ])
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
@@ -113,6 +125,7 @@ class ViewController: UIViewController {
         startNewRound()
         setup()
         layout()
+        setupSlider()
     }
 }
 
@@ -121,24 +134,39 @@ extension ViewController {
     
     @objc func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+          title = "Perfect!"
+          points += 100
+        } else if difference < 5 {
+          title = "You almost had it!"
+          if difference == 1 {
+            points += 50
+          }
+        } else if difference < 10 {
+          title = "Pretty good!"
+        } else {
+          title = "Not even close..."
+        }
         score += points
-        
         let message = "You scored \(points) points"
-        
+
         let alert = UIAlertController(
-            title: "Hello world",
-            message: message,
-            preferredStyle: .alert)
-        
+          title: title,
+          message: message,
+          preferredStyle: .alert)
+
         let action = UIAlertAction(
-            title: "OK",
-            style: .default) {_ in 
-                self.startNewRound()
-            }
-        
+          title: "OK",
+          style: .default) {_ in
+          self.startNewRound()
+        }
+
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
     }
     
     @objc func sliderValueChanged(_ sender: UISlider) {
@@ -146,6 +174,7 @@ extension ViewController {
     }
     
     @objc func restartGame() {
+        print("ok")
         score = 0
         round = 0
         startNewRound()
@@ -175,24 +204,50 @@ extension ViewController {
 extension ViewController {
     
     private func setup() {
-        view.backgroundColor = .systemBackground
-        
         slider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
         showAlertButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
-        startButton.addTarget(self, action: #selector(restartGame), for: .touchUpInside)
+        contentStartButton.addTarget(self, action: #selector(restartGame), for: .touchUpInside)
+    }
+    
+    private func setupSlider() {
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable =
+        trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable =
+        trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     
     private func layout() {
+        view.addSubview(backgroundImageView)
         view.addSubview(titleLabel)
         view.addSubview(goalLabel)
         view.addSubview(sliderStackView)
         view.addSubview(showAlertButton)
-        view.addSubview(infoImageView)
+        view.addSubview(contentStartButton)
+        view.addSubview(startButton)
         view.addSubview(hStackView)
+        
+        startButton.layer.zPosition = 1
+        
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(30)
-            make.centerX.equalToSuperview()
+            make.centerX.equalTo(view.snp.centerX)
         }
         
         goalLabel.snp.makeConstraints { make in
@@ -208,12 +263,21 @@ extension ViewController {
         
         showAlertButton.snp.makeConstraints { make in
             make.top.equalTo(sliderStackView.snp.bottom).offset(50)
-            make.leading.equalTo(view.snp.leading).offset(80)
-            make.trailing.equalTo(view.snp.trailing).offset(-80)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(100)
+            make.height.equalTo(38)
         }
         
-        infoImageView.snp.makeConstraints { make in
-            make.size.equalTo(30)
+        contentStartButton.snp.makeConstraints { make in
+            make.height.width.equalTo(40)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.height.width.equalTo(20)
+            make.top.equalTo(contentStartButton.snp.top).offset(5)
+            make.bottom.equalTo(contentStartButton.snp.bottom).offset(-5)
+            make.leading.equalTo(contentStartButton.snp.leading).offset(5)
+            make.trailing.equalTo(contentStartButton.snp.trailing).offset(-5)
         }
         
         hStackView.snp.makeConstraints { make in
@@ -223,4 +287,6 @@ extension ViewController {
         }
     }
 }
+
+
 
