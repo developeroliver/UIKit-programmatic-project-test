f9d9759e3244bbd0846f5e3b7fde29374cf08a30
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-27 20:01:24 +0200

    remove storyboard

---
+++ We Split/We Split/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  We Split
+//
+//  Created by olivier geiger on 27/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
